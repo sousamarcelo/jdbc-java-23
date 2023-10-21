a3b476d191c8364e999e4d8d2c8b7c7318d9010a
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-21 17:35:45 -0300

    adicionando um vendedor na tabela de vendedores utilizando jdbc

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
@@ -11,30 +12,40 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		
 		try {
+			//estabelecendo a conexão
 			conn = DB.getConnection();
 			
-			st = conn.createStatement();
-			
-			rs = st.executeQuery(
-					"select * from department"
+			// criando o statment utiliando o placehoder para cada coluna a ser preenchida
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?) "							
 					);
-			//percorrendo o resultset que tem um formato de tabela
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-			}
 			
-		}catch 	(SQLException e) {
+			// cada set abaixo substitui um interrogação por ordem
+			st.setString(1, "Carl Purple");
+			st.setString(2, "Carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime())); // por se tratar de consulta no banco e para não geração confusão nos importes foi utilizado nome qualificade da biblioteca Date que deve ser do pacote slq
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);			
+			
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
-			DB.closeResultSet(rs);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
 			DB.closeStatement(st);
-			DB.closeConnection();
+			DB.getConnection();
 		}
 		
 
