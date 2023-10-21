40ae92928130040858e0dac382b014b07af5cf8a
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-10-21 17:09:27 -0300

    fazendo a primeira consulta no bando via codigo java, criado metodos statico de fechamento das variaveis de conexão

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,8 +11,32 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery(
+					"select * from department"
+					);
+			//percorrendo o resultset que tem um formato de tabela
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+			
+		}catch 	(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
 
 	}
 
